9f7e98c0cb53373926c5325aa32407165ff320b0
Clément <clbourges1@poste.isima.fr>
2017-11-13 17:27:52 +0100

    Fin du php

---
+++ public_html/footer.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+     <div class="container">
+        	<div class="text-center center-block">
+                	<a href="https://www.facebook.com/"><i class="fa fa-facebook-square fa-3x social"></i></a>
+	            	<a href="https://maps.google.fr/"><i class="fa fa-map-marker fa-3x social"></i></a>
+	            	<a href="https://www.isima.fr/~clbourges1/authadmin.php"><i class="fa fa-lock fa-3x social"></i></a>
+		</div>
+	</div>
+ </footer>
+
+
+  </body>
+</html>
+
+</body>
+</html>
---
+++ public_html/res_auth_user.php
@@ -0,0 +1,48 @@
+<?php 
+	session_start();
+?>
+<html>
+<meta charset="utf-8">
+<body>
+
+<br>
+<?php 
+
+function Auth ($log,$pass)
+{
+	$bool=0;
+	$fic=fopen("./db/userpass.txt", "r");
+	$logpass=$log.";".$pass."";
+	while (!feof($fic) AND $bool==0)
+	{
+		$buffer=fgets($fic);
+		$buffer=substr($buffer,0,strlen($buffer)-1);
+		if(strcmp($logpass,$buffer)==0)
+		{
+			$bool=1;
+		
+		}
+		$i=$i+1;
+	}
+	fclose($fic);
+	return $bool;
+}
+
+
+
+if (Auth($_POST["login"],$_POST["password"])==1)
+{
+	echo "Accès autorisé";
+	$_SESSION['U']=1;
+	header('Location: https://www.isima.fr/~clbourges1/agenda.php');
+  	exit();
+}
+else
+{
+	echo "Accès refusé";
+}
+
+?>
+
+</body>
+</html>
