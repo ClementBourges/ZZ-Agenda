5e724d57783383ae38bc5278f4548b6f5a7cc48c
sudoku <clement.laneres@free.fr>
2017-11-20 19:20:27 +0100

    ajout ordre

--- public_html/ecriture.php
+++ public_html/ecriture.php
@@ -1,14 +1,35 @@
 <?php
 
-function Ecriture ($titre,$lieu,$speaker,$sujet)
+function Ecriture ($date,$heure,$titre,$lieu,$speaker,$sujet)
 {
-	$fic=fopen("./db/events.txt", "a+");
-	fputs($fic,"$titre;$lieu;$speaker;$sujet");
+	$fic=fopen("./db/events.txt", "r+");
+	$temp="./db/fictemp.txt";
+	$fic2=fopen($temp,"x+");
+	echo $date;
+	echo "......";
+	$temp=$date."-".$heure;
+	$e=0;
+	while (!feof($fic))
+	{
+		$ligne=fgets($fic);
+		$tableau=explode(";",$ligne);
+		if (strcmp($tableau[1],$temp)<0 &&!$e)
+		{
+			echo $temp;
+			fwrite($fic2,"$titre;$temp;$lieu;$speaker;$sujet;\n");
+			fwrite($fic2,$ligne);
+			$e=1;
+			echo "zrgojsgjjlid";
+		}
+		fwrite($fic2,$ligne);
+	}
+	fclose($fic2);
 	fclose($fic);
+	rename($temp,"events.txt");
 }
+
 ?>
 <?php
-$fic=fopen("./db/events.txt", "a+");
-Ecriture($_POST['Titre'],$_POST['Lieu'],$_POST['Speaker'],$_POST['Sujet']);
-header('Location: ./agenda.php');
+Ecriture($_POST['Date'],$_POST['Heure'],$_POST['Titre'],$_POST['Lieu'],$_POST['Speaker'],$_POST['Sujet']);
+/*header('Location: ./agenda.php');*/
 ?>
