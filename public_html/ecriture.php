bced8f28eaf7791f7900990741a58ff637e63c1c
sudoku <clement.laneres@free.fr>
2017-11-20 17:14:58 +0100

    ajout ecriture php

--- public_html/ecriture.php
+++ public_html/ecriture.php
@@ -4,12 +4,11 @@ function Ecriture ($titre,$lieu,$speaker,$sujet)
 {
 	$fic=fopen("./db/events.txt", "a+");
 	fputs($fic,"$titre;$lieu;$speaker;$sujet");
-	fputs("/n");
 	fclose($fic);
 }
 ?>
 <?php
 $fic=fopen("./db/events.txt", "a+");
 Ecriture($_POST['Titre'],$_POST['Lieu'],$_POST['Speaker'],$_POST['Sujet']);
-header('Location: https://www.isima.fr/~cllaneres/agenda.php');
+header('Location: ./agenda.php');
 ?>
