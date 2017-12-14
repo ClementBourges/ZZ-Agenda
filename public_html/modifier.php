5111772ff085db7d4f9c14642683d4be7841b8d6
Clément <clbourges1@poste.isima.fr>
2017-12-14 16:28:31 +0100

    Modification événement

--- public_html/modifier.php
+++ public_html/modifier.php
@@ -1,10 +1,14 @@
 <?php
 if($_SESSION['A']==1 OR 1)
 {
+setcookie();
 include('./header_deco.php'); 
 $hm=explode(":",$_COOKIE['heure']);
 $heurmin=$hm[0].$hm[1];
-setcookie('heure',$heurmin);
+$datetab=explode("/",$_COOKIE['date']);
+$dateformatee=$datetab[2]."-".$datetab[1]."-".$datetab[0];
+echo $dateformatee;
+
 ?>
 
 <body>
@@ -13,7 +17,7 @@ setcookie('heure',$heurmin);
     <h1>Modification de l'événement</h1> 
       <form action="ecriture.php" method="post" class="form-signin">
 		 <label for="input" class="sr-only">Date</label>
-        <input name="Date" class="form-control" placeholder="Date" type="date" value=<?php echo $_COOKIE['date'] ?> required autofocus>
+        <input name="Date" class="form-control" placeholder="Date" type="date" value=<?php echo $dateformatee ?> required autofocus>
         <label for="input" class="sr-only" >Heure</label>
         <input name="Heure" class="form-control" placeholder="Heure" value=<?php echo $_COOKIE['heure'] ?> required autofocus>
         <label for="input" class="sr-only">Titre</label>
@@ -26,9 +30,17 @@ setcookie('heure',$heurmin);
         <input name="Speaker" class="form-control" placeholder="Speaker" value=<?php echo $_COOKIE['speaker'] ?> required autofocus>
         <label for="input" class="sr-only">Sujet</label>
         <input type="Sujet" name="Sujet" id="inputPassword" class="form-control" placeholder="Action" value=<?php echo $_COOKIE['description'] ?> required>
-        <div class="checkbox">
+	<div class="checkbox">
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Modifier</button>
+	<label for="input" class="sr-only">Couleur</label>
+	<input type="color" name="Couleur" id="inputPassword" value=<?php echo $_COOKIE['couleur'] ?> class="form-control" placeholder="Couleur" required>       
+	<div class="checkbox">
+        </div>
+	<form action="supprimer.php" method="post" class="form-signin">
+			<button class="btn btn-lg btn-primary btn-block" type="submit">Supprimer</button>
+	</form>
+	<form action="ecriture.php" method="post" class="form-signin">
+		<button class="btn btn-lg btn-primary btn-block" type="submit">Ajouter</button>
       </form>
 <?php
 $_POST['Date'];
