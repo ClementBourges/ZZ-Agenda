11c19cfcd81ad65709ed28dab928b92030bfea37
ClementBourges <clement.bourges@hotmail.com>
2017-12-16 18:02:58 +0100

    CI Ajout event

--- public_html/tests/tests.php
+++ public_html/tests/tests.php
@@ -49,7 +49,7 @@ class Tests extends TestCase
 	
 	public function test_new_event()
 	{
-		//AjoutEvenement("./events.txt","1996-10-22","14:50","Conférence","Clermont-Ferrand","Speaker","Sujet","#a82a2a");
+		AjoutEvenement("./events.txt","1996-10-22","14:50","Conférence","Clermont-Ferrand","Speaker","Sujet","#a82a2a");
 		$fic=fopen("./events.txt", "r");
 		$ligne=fgets($fic);		
 		$this->assertEquals($ligne,"199610221450;Conférence;Clermont-Ferrand;Speaker;Sujet;#a82a2a;\n");
