096466e8ec5a3d25ee1bdbacb1075574299f0c14
ClementBourges <clement.bourges@hotmail.com>
2017-12-16 18:44:44 +0100

    CI Ajout event

--- public_html/tests/tests.php
+++ public_html/tests/tests.php
@@ -47,12 +47,38 @@ class Tests extends TestCase
 		echo "Fin des tests d'authentification \n";
         }
 	
-	public function test_new_event()
+	public function test_new_event_fichier_vide()
 	{
-		AjoutEvenement("./events.txt","1996-10-22","14:50","Conference","Clermont-Ferrand","Speaker","Sujet","#a82a2a");
+		AjoutEvenement("./db_tests/events_vide.txt","1996-10-22","14:50","Conference","Clermont-Ferrand","Speaker","Sujet","#a82a2a");
 		$fic=fopen("./events.txt", "r");
 		$ligne=fgets($fic);		
 		$this->assertEquals($ligne,"199610221450;Conference;Clermont-Ferrand;Speaker;Sujet;#a82a2a;\n");
+		fclose($fic);
+	}	
+	public function test_new_event_event_seul()
+	{
+		AjoutEvenement("./db_tests/event_seul.txt","1996-10-22","14:50","Conference","Clermont-Ferrand","Speaker","Sujet","#a82a2a");
+		$fic=fopen("./db_tests/event_seul.txt", "r");
+		$ligne=fgets($fic);		
+		$this->assertEquals($ligne,"199610221450;Conference;Clermont-Ferrand;Speaker;Sujet;#a82a2a;\n");
+		$ligne=fgets($fic);		
+		$this->assertEquals($ligne,"199710221450;Conference;Clermont-Ferrand;Speaker;Sujet;#a82a2a;\n");
+		fclose($fic);
+	}
+	public function test_new_event_grande_liste()
+	{
+		AjoutEvenement("./db_tests/grande_liste_event.txt","1996-10-22","14:50","Conference","Clermont-Ferrand","Speaker","Sujet","#a82a2a");
+		$fic=fopen("./db_tests/grande_liste_event.txt", "r");
+		$ligne=fgets($fic);		
+		$this->assertEquals($ligne,"199410221450;Conference;Clermont-Ferrand;Speaker;Sujet;#a82a2a;\n");
+		$ligne=fgets($fic);		
+		$this->assertEquals($ligne,"199510221450;Conference;Clermont-Ferrand;Speaker;Sujet;#a82a2a;\n");
+		$ligne=fgets($fic);
+		$this->assertEquals($ligne,"199610221450;Conference;Clermont-Ferrand;Speaker;Sujet;#a82a2a;\n");	
+		$ligne=fgets($fic);		
+		$this->assertEquals($ligne,"199710221450;Conference;Clermont-Ferrand;Speaker;Sujet;#a82a2a;\n");
+		$ligne=fgets($fic);		
+		$this->assertEquals($ligne,"199810221450;Conference;Clermont-Ferrand;Speaker;Sujet;#a82a2a;\n");
 		fclose($fic);
 	}
 	public function test_delete_event()
