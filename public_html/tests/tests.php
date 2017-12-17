c77df81bd3acf63ba95aa132a5021aa857ed7f7c
ClementBourges <clement.bourges@hotmail.com>
2017-12-17 14:22:56 +0100

    CI Ajout commentaires

--- public_html/tests/tests.php
+++ public_html/tests/tests.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class Tests extends TestCase
 {
 	
-	public function test_True_Auth() /* Auth() return 1 for a right login/pass combination using userpass.txt database */
+	public function debut()
 	{
 		$a="machin";
 		$b="bidule";
